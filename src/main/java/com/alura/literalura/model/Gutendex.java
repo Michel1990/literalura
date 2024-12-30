a14660d5e844b7b9c3f995646e99fa62754777c4
Michel Pereira <maiklim@yahoo.com.br>
2024-12-30 19:27:11 -0300

    Initial commit

---
+++ src/main/java/com/alura/literalura/model/Gutendex.java
@@ -0,0 +1,11 @@
+package com.alura.literalura.model;
+
+import java.util.List;
+
+public record Gutendex(
+        int count,
+        String next,
+        String previous,
+        List<DadosLivro> results
+) {
+}
